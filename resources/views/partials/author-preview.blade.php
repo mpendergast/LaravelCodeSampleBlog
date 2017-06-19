bc5b5dee6c86e5a0a9fed92632b6e2fa9d415fde
Martin Pendergast <mmpenderg@gmail.com>
2017-06-18 23:26:56 -0400

    in-progress article functionality

---
+++ resources/views/partials/author-preview.blade.php
@@ -0,0 +1,7 @@
+<div class="panel panel-info">
+    <div class="panel-heading"><a>{{$preview['authorName']}}</a></div>
+    <div class="panel-body">
+        <b><a> {{$preview['articleTitle']}}</a></b>
+        {{ $preview['articleContent'] }}
+    </div>
+</div>
